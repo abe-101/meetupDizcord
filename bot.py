0c9fffce9014d6937b37183762f696e7e06c70e3
Abe <abe@habet.dev>
2023-07-12 22:23:50 -0400

    change status

--- bot.py
+++ bot.py
@@ -171,7 +171,7 @@ async def status_task() -> None:
     """
     Setup the game status task of the bot.
     """
-    statuses = ["with you!", "with Krypton!", "with humans!"]
+    statuses = ["🌐 Exploring Meetup integration!", "🎮 Engaging with Discord!", "🤝 Connecting communities!"]
     await bot.change_presence(activity=discord.Game(random.choice(statuses)))
 
 
